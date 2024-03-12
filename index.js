1de58b8f74e0970c3883d88f79f7f0831754b231
Shreya Rani <69623298+shreya9902@users.noreply.github.com>
2024-03-12 13:05:24 +0530

    Demo js files added

---
+++ index.js
@@ -0,0 +1,85 @@
+import { StyledLandingPage, Group1, YourEmailHere, Frame3, Submit, N30DaysMoneyBackGuarantee, EnrollYourselfNowJustDropYourEmaili, CreativityMasterclass, DesignStudio, Rectangle1, Frame4, Image4, Image5, Image3, Image6, Image7, TrustedByOver10000Companies, TrustedByOver, N10000, NCompanies, Frame2, Services, Products, Resources, ContectUs, Ellipse1, Frame1, LetsTalk, N1281282982Teenageblackboypngtransp, Group4, Illustrator, OutlinedMnemonicsLogos2, Vector8, Vector9, Ellipse2, Group5, Xd, OutlinedMnemonicsLogos3, Vector11, Vector12, Iphone11ProMax1, TopRatedCourseFrom5000Students, N5000, NStudents, TopRatedCourseFrom, Group2, Photoshop, OutlinedMnemonicsLogos, Vector2, Vector3, Group3, Aftereffects, Outlinedmnemonicslogos, Vector5, Vector6 } from "./styles";
+
+export default function LandingPage(props) {
+  return (
+    <StyledLandingPage>
+      <Group1>
+        <YourEmailHere>Your E-mail here</YourEmailHere>
+        <Frame3>
+          <Submit>SUBMIT</Submit>
+        </Frame3>
+      </Group1>
+      <N30DaysMoneyBackGuarantee>30 Days Money Back Guarantee.</N30DaysMoneyBackGuarantee>
+      <EnrollYourselfNowJustDropYourEmaili>Enroll yourself now. Just drop your email-id here and our team will contact you with class details.</EnrollYourselfNowJustDropYourEmaili>
+      <CreativityMasterclass>{`Creativity
+Masterclass`}</CreativityMasterclass>
+      <DesignStudio>Design Studio</DesignStudio>
+      <Rectangle1>
+        <Frame4>
+          <Image4 backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/589e1faf-6ccf-4adf-942a-7ccf51ccb428.png`} />
+          <Image5 backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/71b81ea8-b2f2-47d3-a958-019a971bc8e3.png`} />
+          <Image3 backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/f398768c-442f-4e42-aea7-03071da0818e.png`} />
+          <Image6 backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/113f8e9d-7297-488b-b097-a3444f92df91.png`} />
+          <Image7 backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/565038b2-0d1c-46e9-b48c-ad2794d54485.png`} />
+        </Frame4>
+      </Rectangle1>
+      <TrustedByOver10000Companies>
+        <TrustedByOver>Trusted by over </TrustedByOver>
+        <N10000>10,000</N10000>
+        <NCompanies>Companies</NCompanies>
+      </TrustedByOver10000Companies>
+      <Frame2>
+        <Services>Services</Services>
+        <Products>Products</Products>
+        <Resources>Resources</Resources>
+        <ContectUs>Contect Us</ContectUs>
+      </Frame2>
+      <Ellipse1 />
+      <Frame1>
+        <LetsTalk>Lets Talk</LetsTalk>
+      </Frame1>
+      <N1281282982Teenageblackboypngtransp backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/441fb770-01a4-4c40-b223-00bf87ebfc64.png`}>
+        <Group4>
+          <Illustrator backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/d911d627-91f1-41bd-8737-b3f10db108ef.png`}>
+            <OutlinedMnemonicsLogos2>
+              <Vector8 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/4e4abacb-33ed-40a0-8fcc-ded7de3e311f.png`} />
+              <Vector9 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/a416205d-885c-4379-a6ca-b24debe62b4e.png`} />
+            </OutlinedMnemonicsLogos2>
+          </Illustrator>
+        </Group4>
+        <Ellipse2 />
+        <Group5>
+          <Xd backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/8523648e-3126-4538-bdf3-f3dbc3a36495.png`}>
+            <OutlinedMnemonicsLogos3>
+              <Vector11 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/f96458d8-0dd5-4969-99c9-5c06c78998a6.png`} />
+              <Vector12 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/15436112-3600-48cc-a08b-effbab8d8c6d.png`} />
+            </OutlinedMnemonicsLogos3>
+          </Xd>
+        </Group5>
+      </N1281282982Teenageblackboypngtransp>
+      <Iphone11ProMax1>
+        <TopRatedCourseFrom5000Students>
+          <N5000>5,000+</N5000>
+          <NStudents>Students</NStudents>
+          <TopRatedCourseFrom>Top Rated Course From </TopRatedCourseFrom>
+        </TopRatedCourseFrom5000Students>
+      </Iphone11ProMax1>
+      <Group2>
+        <Photoshop backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/d353e584-2836-48c9-99bc-51002d124758.png`}>
+          <OutlinedMnemonicsLogos>
+            <Vector2 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/3e780e16-ca8d-4cc5-8af6-72e2120be427.png`} />
+            <Vector3 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/360ada93-c89f-48a1-8ab4-4da14e7746d5.png`} />
+          </OutlinedMnemonicsLogos>
+        </Photoshop>
+      </Group2>
+      <Group3>
+        <Aftereffects backgroundImage={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/e2a8a2e3-2b30-47af-9427-44e9d727889a.png`}>
+          <Outlinedmnemonicslogos>
+            <Vector5 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/99e5dcea-b429-4045-9dc6-fb326d966a90.png`} />
+            <Vector6 src={`https://assets.rapidream.com/project/fe9c136b-3a28-4591-820e-07d72c3410f0/assets/72bd0eb7-087c-47a9-94c2-646b7e936fa4.png`} />
+          </Outlinedmnemonicslogos>
+        </Aftereffects>
+      </Group3>
+    </StyledLandingPage>
+  );
+}
---
+++ styles.js
@@ -0,0 +1,672 @@
+import styled from "styled-components";
+
+export const StyledLandingPage = styled.div`
+  width: 100%;
+  mask-type: ALPHA;
+  min-height: 1117px;
+  background: rgba(249, 251, 255, 1);
+  position: relative;
+`;
+
+export const Group1 = styled.div`
+  top: 657px;
+  left: 6.02%;
+  width: 39.64%;
+  filter: drop-shadow(6px 8px 16px rgba(0, 0, 0, 0.08));
+  mask-type: ALPHA;
+  min-height: 104px;
+  background: white;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 16px;
+`;
+
+export const YourEmailHere = styled.span`
+  color: rgba(184.38, 184.38, 184.38, 1);
+  opacity: 0.5;
+  font-size: 24px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 500;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+  margin-left: 2.61%;
+  align-self: flex-start;
+  margin-top: 38px;
+`;
+
+export const Frame3 = styled.div`
+  width: 30.08%;
+  mask-type: ALPHA;
+  min-height: 104px;
+  background: rgba(153.69, 210.37, 125.35, 1);
+  margin-left: auto;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Submit = styled.span`
+  color: white;
+  font-size: 31px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+  margin-top: 17px;
+`;
+
+export const N30DaysMoneyBackGuarantee = styled.span`
+  top: 490px;
+  left: 6.02%;
+  color: black;
+  font-size: 32px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+  position: absolute;
+`;
+
+export const EnrollYourselfNowJustDropYourEmaili = styled.span`
+  top: 569px;
+  left: 6.09%;
+  color: rgba(113.69, 113.69, 113.69, 1);
+  opacity: 0.8;
+  font-size: 26px;
+  mask-type: ALPHA;
+  max-width: 659.375px;
+  font-family: Montserrat;
+  font-weight: 500;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+  position: absolute;
+`;
+
+export const CreativityMasterclass = styled.span`
+  top: 244px;
+  left: 6.19%;
+  color: black;
+  font-size: 88px;
+  mask-type: ALPHA;
+  max-width: 705.37px;
+  font-family: Montserrat;
+  font-weight: 800;
+  white-space: pre-wrap;
+  letter-spacing: 0.02em;
+  text-decoration: NONE;
+  position: absolute;
+  width: 34.02%;
+`;
+
+export const DesignStudio = styled.span`
+  top: 63px;
+  left: 6.19%;
+  color: black;
+  font-size: 42px;
+  mask-type: ALPHA;
+  font-family: Iceland;
+  font-weight: 400;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+  position: absolute;
+`;
+
+export const Rectangle1 = styled.div`
+  top: 1000px;
+  left: 12.09%;
+  width: 78.7%;
+  mask-type: ALPHA;
+  min-height: 117px;
+  background: rgba(254, 217, 22, 1);
+  border-radius: 24px 24px 0px 0px;
+  border-top-left-radius: 24px;
+  border-top-right-radius: 24px;
+  position: absolute;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Frame4 = styled.div`
+  width: 87.94000000000001%;
+  mask-type: ALPHA;
+  min-height: 65.38px;
+  display: flex;
+  flex-wrap: wrap;
+  min-width: min-content;
+  align-self: center;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Image4 = styled.div`
+  width: 200px;
+  mask-type: ALPHA;
+  max-width: 240px;
+  min-height: 65.38px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  min-width: 160px;
+`;
+
+export const Image5 = styled.div`
+  width: 160px;
+  mask-type: ALPHA;
+  max-width: 192px;
+  min-height: 55px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  min-width: 128px;
+  margin-left: 5.08%;
+`;
+
+export const Image3 = styled.div`
+  width: 184px;
+  mask-type: ALPHA;
+  max-width: 220.8px;
+  min-height: 64px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  min-width: 147.2px;
+  margin-left: 5.08%;
+`;
+
+export const Image6 = styled.div`
+  width: 216px;
+  mask-type: ALPHA;
+  max-width: 259.2px;
+  min-height: 58px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  min-width: 172.8px;
+  margin-left: 5.08%;
+`;
+
+export const Image7 = styled.div`
+  width: 180px;
+  mask-type: ALPHA;
+  max-width: 216px;
+  min-height: 53px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  min-width: 144px;
+  margin-left: 5.08%;
+`;
+
+export const TrustedByOver10000Companies = styled.div`
+  top: 938.3px;
+  left: 33.62%;
+  width: 33.19%;
+  mask-type: ALPHA;
+  min-height: 31.14px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  position: absolute;
+  display: inline;
+`;
+
+export const TrustedByOver = styled.span`
+  color: rgba(184.38, 184.38, 184.38, 1);
+  display: inline;
+  font-size: 32px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+`;
+
+export const N10000 = styled.span`
+  color: rgba(154, 210, 125, 1);
+  display: inline;
+  font-size: 32px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+`;
+
+export const NCompanies = styled.span`
+  color: rgba(184.38, 184.38, 184.38, 1);
+  display: inline;
+  font-size: 32px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0em;
+  text-decoration: NONE;
+`;
+
+export const Frame2 = styled.div`
+  top: 72px;
+  left: 45.19%;
+  width: 36.11%;
+  mask-type: ALPHA;
+  min-height: 24.02px;
+  position: absolute;
+  display: flex;
+  flex-wrap: wrap;
+  min-width: min-content;
+  flex-direction: row;
+  align-items: start;
+`;
+
+export const Services = styled.span`
+  color: black;
+  font-size: 20px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 500;
+  white-space: pre-wrap;
+  letter-spacing: 0.02em;
+  text-decoration: NONE;
+`;
+
+export const Products = styled.span`
+  color: black;
+  font-size: 20px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 500;
+  white-space: pre-wrap;
+  letter-spacing: 0.02em;
+  text-decoration: NONE;
+  margin-left: 11.58%;
+`;
+
+export const Resources = styled.span`
+  color: black;
+  font-size: 20px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 500;
+  white-space: pre-wrap;
+  letter-spacing: 0.02em;
+  text-decoration: NONE;
+  margin-left: 11.1%;
+`;
+
+export const ContectUs = styled.span`
+  color: black;
+  font-size: 20px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 500;
+  white-space: pre-wrap;
+  letter-spacing: 0.02em;
+  text-decoration: NONE;
+  margin-left: 11.08%;
+`;
+
+export const Ellipse1 = styled.div`
+  top: 177px;
+  left: 53.41%;
+  width: 25.81%;
+  mask-type: ALPHA;
+  min-height: 446px;
+  background: rgba(251, 227, 178, 1);
+  border-radius: 9999px;
+  position: absolute;
+`;
+
+export const Frame1 = styled.div`
+  top: 56px;
+  left: 85.76%;
+  width: 8.22%;
+  mask-type: ALPHA;
+  min-height: 56px;
+  background: rgba(254, 217, 22, 1);
+  position: absolute;
+  display: flex;
+  max-width: 170.4px;
+  min-width: 128.230625px;
+  align-items: start;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const LetsTalk = styled.span`
+  color: black;
+  font-size: 20px;
+  mask-type: ALPHA;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0.02em;
+  text-decoration: NONE;
+  margin-left: 2.03%;
+`;
+
+export const N1281282982Teenageblackboypngtransp = styled.div`
+  top: 84px;
+  left: 54.92%;
+  width: 40.05%;
+  mask-type: ALPHA;
+  min-height: 810px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  position: absolute;
+`;
+
+export const Group4 = styled.div`
+  top: 195px;
+  left: 66.62%;
+  width: 10.4%;
+  filter: drop-shadow(5px 6px 12px rgba(0, 0, 0, 0.10));
+  mask-type: ALPHA;
+  min-height: 72px;
+  background: white;
+  border-radius: 9999px;
+  position: absolute;
+  display: flex;
+  max-width: 86.4px;
+  min-width: 57.6px;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Illustrator = styled.div`
+  width: 70.65%;
+  mask-type: ALPHA;
+  max-width: 41.843999999999994px;
+  min-height: 34px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  display: flex;
+  min-width: 27.895999999999997px;
+  align-items: center;
+  justify-content: center;
+  padding-left: 7px;
+  padding-top: 8px;
+  padding-right: 8px;
+  padding-bottom: 10px;
+`;
+
+export const OutlinedMnemonicsLogos2 = styled.div`
+  width: 98.4%;
+  mask-type: ALPHA;
+  min-height: 16.49px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const Vector8 = styled.img`
+  width: 14.33px;
+  height: 15.33px;
+  max-width: 17.196px;
+  min-width: 11.464px;
+`;
+
+export const Vector9 = styled.img`
+  width: 3.62px;
+  height: 16.49px;
+  max-width: 4.344px;
+  min-width: 2.896px;
+`;
+
+export const Ellipse2 = styled.div`
+  top: 316px;
+  left: 33.24%;
+  width: 64.45%;
+  mask-type: ALPHA;
+  min-height: 446px;
+  background: rgba(210, 248, 255, 1);
+  border-radius: 9999px;
+  position: absolute;
+`;
+
+export const Group5 = styled.div`
+  top: 413px;
+  left: 89.6%;
+  width: 10.4%;
+  filter: drop-shadow(5px 6px 12px rgba(0, 0, 0, 0.10));
+  mask-type: ALPHA;
+  min-height: 72px;
+  background: white;
+  border-radius: 9999px;
+  position: absolute;
+  display: flex;
+  max-width: 86.4px;
+  min-width: 57.6px;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Xd = styled.div`
+  width: 70.65%;
+  mask-type: ALPHA;
+  max-width: 41.843999999999994px;
+  min-height: 34px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  display: flex;
+  min-width: 27.895999999999997px;
+  align-items: center;
+  justify-content: center;
+  padding-left: 5px;
+  padding-top: 7px;
+  padding-right: 5px;
+  padding-bottom: 10px;
+`;
+
+export const OutlinedMnemonicsLogos3 = styled.div`
+  width: 99.57%;
+  mask-type: ALPHA;
+  min-height: 16.82px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const Vector11 = styled.img`
+  width: 13.59px;
+  height: 15.28px;
+  max-width: 16.308px;
+  min-width: 10.872px;
+`;
+
+export const Vector12 = styled.img`
+  width: 10.85px;
+  height: 16.82px;
+  max-width: 13.02px;
+  min-width: 8.68px;
+`;
+
+export const Iphone11ProMax1 = styled.div`
+  top: 743px;
+  left: 50.17%;
+  width: 45.54%;
+  mask-type: ALPHA;
+  box-shadow: inset -5px -5px 250px rgba(255, 255, 255, 0.02) inset;
+  min-height: 163px;
+  position: absolute;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const TopRatedCourseFrom5000Students = styled.div`
+  mask-type: ALPHA;
+  min-height: 55.22px;
+  text-align: center;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  margin-left: -1.14%;
+  margin-top: 23.71px;
+  display: inline;
+`;
+
+export const N5000 = styled.span`
+  color: rgba(254, 217, 22, 1);
+  display: inline;
+  font-size: 26px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0.05em;
+  text-decoration: NONE;
+`;
+
+export const NStudents = styled.span`
+  color: white;
+  display: inline;
+  font-size: 26px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0.05em;
+  text-decoration: NONE;
+`;
+
+export const TopRatedCourseFrom = styled.span`
+  color: white;
+  display: inline;
+  font-size: 26px;
+  font-family: Montserrat;
+  font-weight: 600;
+  white-space: pre-wrap;
+  letter-spacing: 0.05em;
+  text-decoration: NONE;
+`;
+
+export const Group2 = styled.div`
+  top: 417px;
+  left: 51.79%;
+  width: 4.17%;
+  filter: drop-shadow(5px 6px 12px rgba(0, 0, 0, 0.10));
+  mask-type: ALPHA;
+  min-height: 72px;
+  background: white;
+  border-radius: 9999px;
+  position: absolute;
+  display: flex;
+  max-width: 86.4px;
+  min-width: 57.6px;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Photoshop = styled.div`
+  width: 70.65%;
+  mask-type: ALPHA;
+  max-width: 41.843999999999994px;
+  min-height: 34px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  display: flex;
+  min-width: 27.895999999999997px;
+  align-items: center;
+  justify-content: center;
+  padding-left: 8px;
+  padding-top: 9px;
+  padding-right: 6px;
+  padding-bottom: 10px;
+`;
+
+export const OutlinedMnemonicsLogos = styled.div`
+  width: 100.22999999999999%;
+  mask-type: ALPHA;
+  min-height: 15.69px;
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  justify-content: space-evenly;
+`;
+
+export const Vector2 = styled.img`
+  width: 11px;
+  height: 15.37px;
+  max-width: 13.2px;
+  min-width: 8.8px;
+`;
+
+export const Vector3 = styled.img`
+  width: 8.45px;
+  height: 12.09px;
+  max-width: 10.139999999999999px;
+  min-width: 6.76px;
+  margin-top: 3.59px;
+`;
+
+export const Group3 = styled.div`
+  top: 172px;
+  left: 53.41%;
+  width: 4.17%;
+  filter: drop-shadow(5px 6px 12px rgba(0, 0, 0, 0.10));
+  mask-type: ALPHA;
+  max-width: 86.4px;
+  min-width: 57.6px;
+  min-height: 72px;
+  background: white;
+  border-radius: 9999px;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const Aftereffects = styled.div`
+  width: 70.65%;
+  mask-type: ALPHA;
+  max-width: 41.843999999999994px;
+  min-height: 34px;
+  background-size: cover;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  display: flex;
+  min-width: 27.895999999999997px;
+  align-items: center;
+  justify-content: center;
+  padding-left: 4px;
+  padding-top: 9px;
+  padding-right: 5px;
+  padding-bottom: 10px;
+`;
+
+export const Outlinedmnemonicslogos = styled.div`
+  width: 99.25%;
+  mask-type: ALPHA;
+  min-height: 15.54px;
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  justify-content: space-evenly;
+`;
+
+export const Vector5 = styled.img`
+  width: 14.33px;
+  height: 15.25px;
+  max-width: 17.196px;
+  min-width: 11.464px;
+`;
+
+export const Vector6 = styled.img`
+  width: 10.57px;
+  height: 12.05px;
+  max-width: 12.684000000000001px;
+  min-width: 8.456px;
+  margin-top: 3.49px;
+`;
